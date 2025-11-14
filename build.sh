98bc71256b7ac578f5dbb20369f12a6bb64fe4d3
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-11-14 10:30:30 +0100

    esp32 generic flash

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 SRC_DIR="src"
 BUILD_DIR="dist"
-ENIGMA_PYTHON_VERSION="1.3.1"
+ENIGMA_PYTHON_VERSION="1.3.0"
 
 cd $SRC_DIR
 for file in $(find . | grep -v __pycache__); 
--- flash-esp32_generic.sh
+++ flash-esp32_generic.sh
@@ -5,10 +5,13 @@ export MICROPYTHON_BIN=ESP32_GENERIC-20250911-v1.26.1.bin
 ./build.sh
 
 # Erase the board flash
-python -m esptool --chip esp32 erase_flash    
+python -m esptool --chip esp32 --baud 9600 --port $ESPPORT erase-flash    
 
 # Upload MicroPython firmware
-python -m esptool --baud 460800 write_flash 0x1000 $MICROPYTHON_BIN # Download the MicroPython binary from https://micropython.org/download/
+python -m esptool --baud 460800 --port $ESPPORT write-flash 0x1000 $MICROPYTHON_BIN # Download the MicroPython binary from https://micropython.org/download/
+
+# Wait for reset to be completed
+sleep 10
 
 # Upload Enigma Core (this may take a while)
 echo "Transferring Enigma Core..."
